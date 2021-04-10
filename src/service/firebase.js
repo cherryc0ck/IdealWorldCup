2ecc597c6564058b32d53dff8f8deb1761446cdd
oriharaa <qstar9537@naver.com>
2021-04-10 16:12:05 +0900

    Add firebase

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,14 +1,8 @@
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyDPW8HDtZP_XhOSNR8jPbiDnEgeFEQNjsE",
-    authDomain: "idealworldcup-cc4fa.firebaseapp.com",
-    projectId: "idealworldcup-cc4fa",
-    storageBucket: "idealworldcup-cc4fa.appspot.com",
-    messagingSenderId: "932091844373",
-    appId: "1:932091844373:web:b287f3a21d97ba5e94bb1c",
-    measurementId: "G-7050KYTQGC"
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
