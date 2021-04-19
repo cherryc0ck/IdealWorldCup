ba6b860c5fc5c884a56d96fbde2e7d699acf03b3
oriharaa <qstar9537@naver.com>
2021-04-19 23:24:52 +0900

    delete p tag

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -17,10 +17,6 @@ const Header = ({ authService }) => {
         world-cup
       </h1>
       <Login authService={authService}  />
-      <p>
-      이상형 월드컵을 직접 진행 해보고 만들수도 있어요!
-      당신의 이상형은 누구인가요? 당신이 제일 좋아하는 과일은 무엇이죠?
-      </p>
     </div>
   </header>
   )
