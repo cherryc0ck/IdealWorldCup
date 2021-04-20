7e8955e994f9755719a31d9430f03c440422818c
oriharaa <qstar9537@naver.com>
2021-04-20 21:09:13 +0900

    modify main component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,9 +5,6 @@ const Main = (props) => (
   <main className={styles.main}>
     <ul>
       <li>이상형 월드컵1</li>
-      <li>이상형 월드컵2</li>
-      <li>이상형 월드컵3</li>
-      <li>이상형 월드컵4</li>
     </ul>
   </main>   
 );
