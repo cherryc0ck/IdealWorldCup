b15ab7a7181d6a4cdbe3830b63a795212c878ddd
oriharaa <qstar9537@naver.com>
2021-05-10 21:51:04 +0900

    Add gameItem component

--- src/components/gameItem/game_item.jsx
+++ src/components/gameItem/game_item.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import worldCupData from '../../data/worldCup.json';
 import styles from './game_item.module.css';
 
-const GameItem = (props) => (
-      <div className={styles.container}>
-        
-      </div>
-  );
+const GameItem = ({game}) => {
+
+  return(
+      <li className={styles.game}>
+        <a className={styles.container}>
+          <div className={styles.imgContainer}>
+            <img className={styles.img} src={game.img} />
+          </div>
+          <p className={styles.title}>{game.title}</p>
+          <button className={styles.button}>START</button>
+        </a>
+      </li>
+  )
+};
 
 export default GameItem;
--- src/components/gameList/game_list.jsx
+++ src/components/gameList/game_list.jsx
@@ -3,11 +3,20 @@ import GameItem from '../gameItem/game_item';
 import worldCupData from '../../data/worldCup.json';
 import styles from './game_list.module.css';
 
-const GameList = (props) => (
-    <ul classList={styles.container}>
-      <GameItem/>
+const GameList = (props) => {
+
+  return(
+    <ul className={styles.container}>
+      {worldCupData.category.map(game=>(
+        <GameItem 
+          key={game.index}
+          game={game}
+        />
+      ))}
+
     </ul>
-  );
+    )
+};
 
 
 export default GameList;
