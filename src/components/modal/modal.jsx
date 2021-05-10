0b504b0ed1ec2f1a3809495a5055c9cadd0ea4e8
oriharaa <qstar9537@naver.com>
2021-05-11 01:33:27 +0900

    2 generation and state randomization for World Cup game test

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './modal.module.css';
 
 
 const Modal = ({game, open, close, title}) => {
   
+  const [items, setItems] = useState([]);
+  const [displays, setDisplays] = useState([]);
+  useEffect(()=>{
+    game.items.sort(() => Math.random() - 0.5);
+    setItems(game.items);
+    setDisplays([items[0], items[1]]);
+  }, []);
+
   return (
     <div className={open ? `${styles.openModal}`:`${styles.modal}`}>
       {open ?(
@@ -13,8 +21,16 @@ const Modal = ({game, open, close, title}) => {
             <button className={styles.headerBtn} onClick={close}>&times;</button>
           </header>
           <main className={styles.main}>
-            <img src={game.img} />
-            <img src={game.img} />
+            <div className={styles.container}>
+              {displays.map(display=>{
+                return (
+                  <div key={display.name}>
+                    <img className={styles.img} src={display.src} />
+                    <p className={styles.name}>{display.name}</p>
+                  </div>
+                )
+              })}
+            </div>
           </main>
           <footer className={styles.footer}>
             <button className={styles.footerBtn} onClick={close}>close</button>
