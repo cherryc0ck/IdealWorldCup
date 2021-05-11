3b04dcfcf3cc990065797e1af147660cc8f31461
oriharaa <qstar9537@naver.com>
2021-05-12 02:56:49 +0900

    ModalGame styling and add operation function setting

--- src/components/modalGame/modal_game.jsx
+++ src/components/modalGame/modal_game.jsx
@@ -5,17 +5,39 @@ import styles from './modal_game.module.css';
 const ModalGame = ({game}) => {
   const [items, setItems] = useState([...game.items]);
   const [displays, setDisplays] = useState([]);
+  const [winners, setWinners] = useState([]);
   useEffect(()=>{
     items.sort(() => Math.random() - 0.5);
     setDisplays([items[0], items[1]]);
   },[]);
+
+  const clickHandler = (picker) => () => {
+    if(items.length <= 2){
+      if(winners.length === 0){
+        setDisplays([picker]);
+      }else {
+        let updatedItems = [...winners, picker];
+        setItems(updatedItems);
+        setDisplays([updatedItems[0], updatedItems[1]]);
+        setWinners([]);
+      }
+    }else if(items.length > 2){
+      setWinners([...winners, picker]);
+      setDisplays([items[2], items[3]]);
+      setItems(items.slice(2));
+    }
+  };
 
   return(
     <main className={styles.main}>
       <div className={styles.container}>
         {displays.map(display => {
           return (
-            <div className={styles.pick} key={display.name}>
+            <div 
+              className={styles.pick} 
+              key={display.name}
+              onClick={clickHandler(display)}
+            >
               <div className={styles.imgContainer}>
                 <img className={styles.img} src={display.src} />
               </div>
@@ -23,7 +45,6 @@ const ModalGame = ({game}) => {
             </div>
           )
         })}
-        <img className={styles.vs} src="./images/vss.png" />
       </div>
     </main>
   )
